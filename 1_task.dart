06e9cbf728884a4a684bf7f908723c68f61ca6b7
Maland <maland83@gmail.com>
2025-06-19 00:04:27 +0300

    Chapter 1: Mathematical Operators

--- 1_task.dart
+++ 1_task.dart
@@ -1,1 +1,11 @@
-void main() {}
+void main() {
+  double rectangle_length = 18.4;
+  double rectangle_width = 30.9;
+
+  double s = rectangle_width * rectangle_length;
+  double p = 2 * (rectangle_width + rectangle_length);
+
+  print('Rectangle area is $s');
+
+  print('|x| ${s ~/ 5}');
+}
