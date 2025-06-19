aea90fd7e101708246469496f0801592d01de6b8
Maland <maland83@gmail.com>
2025-06-19 08:41:12 +0300

    Task 7: Switch statement construction

--- 1_task.dart
+++ 1_task.dart
@@ -64,4 +64,35 @@ void main() {
       print("Повторити курс");
     }
   }
+
+  print("Task 7: Switch statement construction");
+  int dayNumber = 6; // змінюй значення від 1 до 7
+
+  switch (dayNumber) {
+    case 1:
+      print("Понеділок");
+      break;
+    case 2:
+      print("Вівторок");
+      break;
+    case 3:
+      print("Середа");
+      break;
+    case 4:
+      print("Четвер");
+      break;
+    case 5:
+      print("П’ятниця");
+      break;
+    case 6:
+      print("Субота");
+      print("Вихідний день 🎉");
+      break;
+    case 7:
+      print("Неділя");
+      print("Вихідний день 🎉");
+      break;
+    default:
+      print("Невірний номер дня");
+  }
 }
